0b7532fb8f0363d9d9b2e0c31ddb187369bf5797
mdasadofficial <mdasadofficial1@gmail.com>
2026-02-24 02:44:39 +0600

    counter Update

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+// const rejectFilterBtn  = document.getElementById('rejected-filter-btn')
+// const interviewFilterBtn  = document.getElementById('interview-filter-btn')
+// const allFilterBtn  = document.getElementById('all-filter-btn')
+// let allCards = document.getElementById('allCards')
+// let totalCount = document.getElementById('totalCount')
+// let interviewCount = document.getElementById('interviewCount')
+// let rejectedCount = document.getElementById('rejectedCount')
+// let TotalJobs = document.getElementById('TotalJobs')
+// let interviewSection = document.getElementById('interviewSection')
+// let rejectedSection = document.getElementById('rejectedSection')
+// let interviewList = []
+// let rejectedList = []
+
 const allFilterBtn = document.getElementById('all-filter-btn')
 const interviewFilterBtn = document.getElementById('interview-filter-btn')
 const rejectFilterBtn = document.getElementById('rejected-filter-btn')
 
+let totalCount = document.getElementById('total');
+let interviewCount = document.getElementById('interview');
+let rejectCount = document.getElementById('reject');
+let interviewList = []
+let rejectedList = []
 
+const allCards = document.getElementById('allCards');
+const jobCount = document.getElementById('job-count');
+function counter() {
+    
+    totalCount.innerText = allCards.children.length
+    jobCount.innerText = allCards.children.length
+    interviewCount.innerText = interviewList.length
+    rejectCount.innerText = rejectedList.length
 
+}
+counter() 
 
 // Toggle Button Style
 function toggleStyle(id) {
@@ -23,8 +51,4 @@ function toggleStyle(id) {
 
     }
 toggleStyle()
-
-
-
-
 
